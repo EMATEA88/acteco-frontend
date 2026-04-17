db6c533a6e7df4813cbb699ea62d9676f1eb827b
Juliana Ngola <juliana@example.com>
2026-04-17 21:05:29 +0100

    fix: remove PWA index.html

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import { Toaster } from "sonner"
 import "./index.css"
-import { registerSW } from "virtual:pwa-register"
 import { NotificationProvider } from "./contexts/NotificationContext"
-
-registerSW({
-  immediate: true,
-  onNeedRefresh() {
-    window.location.reload()
-  },
-  onOfflineReady() {
-    console.log("PWA pronta para offline")
-  }
-})
 
 ReactDOM.createRoot(
   document.getElementById("root")!
